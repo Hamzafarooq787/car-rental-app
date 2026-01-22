9cbb6dcdc00a640d11787537aa3633b5470d7c5f
Hamzafarooq787 <122914428+Hamzafarooq787@users.noreply.github.com>
2026-01-22 18:23:43 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,3 +18,11 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
